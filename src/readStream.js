cc8b4acfd039ac087f3f841ba73af2dab7dfb158
Stephen Hess <stephen.k.hess@gmail.com>
2016-03-07 09:53:16 -0500

    the only thing that index.js uses from the meta files is the id, so i scrapped the entire contents of `readStreamComponents`

--- src/readStream.js
+++ src/readStream.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var batch = require('batchflow');
 var sink = require('through2-sink');
 
-var readStreamComponents = require('./readStreamComponents');
-
 /*
   This function finds all the `latest` files in `meta/`, CSV parses them,
   extracts the required fields, and assigns to a big collection
@@ -12,23 +10,11 @@ var readStreamComponents = require('./readStreamComponents');
 function readData(directory, types, wofRecords, callback) {
   batch(types).parallel(2).each(function(idx, type, done) {
     var csv_parser = parse({ delimiter: ',', columns: true });
-    var is_valid_data_file_path = readStreamComponents.is_valid_data_file_path();
-    var normalize_file_path = readStreamComponents.normalize_file_path();
-    var file_is_readable = readStreamComponents.file_is_readable(directory + 'data/');
-    var json_parse_stream = readStreamComponents.json_parse_stream(directory + 'data/');
-    var filter_incomplete_files_stream = readStreamComponents.filter_incomplete_files_stream();
-    var map_fields_stream = readStreamComponents.map_fields_stream();
 
     fs.createReadStream(directory + 'meta/wof-' + type + '-latest.csv')
     .pipe(csv_parser)
-    .pipe(is_valid_data_file_path)
-    .pipe(normalize_file_path)
-    .pipe(file_is_readable)
-    .pipe(json_parse_stream)
-    .pipe(filter_incomplete_files_stream)
-    .pipe(map_fields_stream)
-    .pipe(sink.obj(function(wofRecord) {
-      wofRecords[wofRecord.id] = wofRecord;
+    .pipe(sink.obj(function(data) {
+      wofRecords[data.id] = { id: data.id };
     }))
     .on('finish', done);
 
