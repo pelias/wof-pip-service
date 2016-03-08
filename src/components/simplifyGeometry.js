1656cbf1b2cbb4e34fa682f5eff7ef69fcd58a0b
Stephen Hess <stephen.k.hess@gmail.com>
2016-03-08 15:41:54 -0500

    extracted geometry simplifying out to a separate street

---
+++ src/components/simplifyGeometry.js
@@ -0,0 +1,48 @@
+var map = require('through2-map');
+var simplify = require('simplify-js');
+var _ = require('lodash');
+
+module.exports.create = function() {
+  // this function extracts the id, name, placetype, hierarchy, and geometry
+  return map.obj(function(wofData) {
+    wofData.geometry = simplifyGeometry(wofData.geometry);
+
+    return wofData;
+
+  });
+}
+
+function simplifyGeometry(geometry) {
+  if( geometry ) {
+    if ('Polygon' === geometry.type) {
+      var coordinates = geometry.coordinates[0];
+      geometry.coordinates[0] = simplifyCoords(coordinates);
+    }
+    else if ('MultiPolygon' === geometry.type) {
+      var polygons = geometry.coordinates;
+      polygons.forEach(function simplify(coordinates, idx) {
+        polygons[idx][0] = simplifyCoords(coordinates[0]);
+      });
+    }
+  }
+
+  return geometry;
+
+}
+
+/**
+ * @param {array} coords A 2D GeoJson-style points array.
+ * @return {array} A slightly simplified version of `coords`.
+ */
+function simplifyCoords( coords ){
+  var pts = coords.map( function mapToSimplifyFmt( pt ){
+    return { x: pt[ 0 ], y: pt[ 1 ] };
+  });
+
+  var simplificationRate = 0.0003;
+  var simplified = simplify( pts, simplificationRate, true );
+
+  return simplified.map( function mapToGeoJsonFmt( pt ){
+    return [ pt.x, pt.y ];
+  });
+}
