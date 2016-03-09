6d7ef0d1b395b7bd0a189d4cabe4d897ec04dff0
Stephen Hess <stephen.k.hess@gmail.com>
2016-03-09 12:26:36 -0500

    fixed comment

--- src/components/isValidId.js
+++ src/components/isValidId.js
@@ -1,7 +1,7 @@
 var filter = require('through2-filter');
 
 module.exports.create = function() {
-  // this method just returns the id property of an object
+  // return true if id length is gte 6, false otherwise
   return filter.obj(function(id) {
     return id.length >= 6;
   });
