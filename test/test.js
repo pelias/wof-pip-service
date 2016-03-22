7724c98a317baeed91c4dc09f185028b94b2c0b4
Stephen Hess <stephen.k.hess@gmail.com>
2016-03-22 11:42:41 -0400

    added filter to remove deprecated records from load

--- test/test.js
+++ test/test.js
@@ -3,3 +3,4 @@ require ('./test/extractIdTest.js');
 require ('./test/isValidIdTest.js');
 require ('./test/loadJSONTest.js');
 require ('./test/simplifyGeometryTest.js');
+require ('./test/filterOutDeprecatedRecords.js');
