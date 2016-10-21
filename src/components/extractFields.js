29f59a1fa30ca2a1f40788aaf7361ce61cedff1c
Diana Shkolnikov <dianashk@gmail.com>
2016-10-21 11:59:47 -0400

    feat: use official lang for name, add Berlin, Germany functional test

--- src/components/extractFields.js
+++ src/components/extractFields.js
@@ -15,7 +15,7 @@ module.exports.create = function() {
     };
 
   });
-}
+};
 
 // this function is used to verify that a US county QS altname is available
 function isUsCounty(wofData) {
@@ -24,17 +24,46 @@ function isUsCounty(wofData) {
         !_.isUndefined(wofData.properties['qs:a2_alt']);
 }
 
+function getOfficialLangName(wofData) {
+  var langs = wofData.properties['wof:lang_x_official'];
+  var lang = langs;
+  if (langs instanceof Array && langs.length > 1) {
+    console.log('more than one lang specified, using first: ', wofData.properties['wof:lang_x_official'], langs[0]);
+    lang = langs[0];
+  }
+
+  return 'name:' + lang + '_x_preferred';
+}
+
 // if this is a US county, use the qs:a2_alt for county
 // eg - wof:name = 'Lancaster' and qs:a2_alt = 'Lancaster County', use latter
 function getName(wofData) {
   if (isUsCounty(wofData)) {
+    //console.log('using qs:a2_alt name');
     return wofData.properties['qs:a2_alt']
+  }
+
+  if (wofData.properties.hasOwnProperty('wof:lang_x_official') && !_.isEmpty(wofData.properties['wof:lang_x_official'])) {
+
+    // build the preferred lang key to use for name, like 'name:deu_x_preferred'
+    var official_lang_key = getOfficialLangName(wofData);
+
+    //console.log('has official lang: ', official_lang_key, wofData.properties['wof:name'], wofData.properties['wof:placetype'], wofData.properties['wof:id']);
+
+    // check if that language is available
+    if (wofData.properties.hasOwnProperty(official_lang_key)) {
+
+      //console.log('using official lang name: ', wofData.properties[official_lang_key], wofData.properties['wof:placetype'], wofData.properties['wof:id']);
+
+      // return the right name if all went well
+      return wofData.properties[official_lang_key];
+    }
   }
 
   if (wofData.properties.hasOwnProperty('wof:label')) {
     return wofData.properties['wof:label'];
   }
 
+  //console.log('using default wof:name');
   return wofData.properties['wof:name'];
-
 }
