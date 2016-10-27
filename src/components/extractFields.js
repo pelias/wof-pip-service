920614695fa12b4c02e7a5190329ab302b2ceb5f
Diana Shkolnikov <dianashk@gmail.com>
2016-10-27 16:46:26 -0400

    refactor and update tests

--- src/components/extractFields.js
+++ src/components/extractFields.js
@@ -1,5 +1,8 @@
+'use strict';
+
 var map = require('through2-map');
 var _ = require('lodash');
+const logger = require('pelias-logger').get('extractFields');
 
 module.exports.create = function() {
   // this function extracts the id, name, placetype, hierarchy, and geometry
@@ -24,46 +27,72 @@ function isUsCounty(wofData) {
         !_.isUndefined(wofData.properties['qs:a2_alt']);
 }
 
-function getOfficialLangName(wofData) {
-  var langs = wofData.properties['wof:lang_x_official'];
-  var lang = langs;
-  if (langs instanceof Array && langs.length > 1) {
-    console.log('more than one lang specified, using first: ', wofData.properties['wof:lang_x_official'], langs[0]);
-    lang = langs[0];
+function getOfficialLangName(wofData, langProperty) {
+  var languages = wofData.properties[langProperty];
+
+  // convert to array in case it is just a string
+  if (!(languages instanceof Array)) {
+    languages = [languages];
   }
 
-  return 'name:' + lang + '_x_preferred';
+  if (languages.length > 1) {
+    logger.info(`more than one ${langProperty} specified`,
+      wofData.properties['wof:lang_x_official'], languages);
+  }
+
+  // for now always just grab the first language in the array
+  return `name:${languages[0]}_x_preferred`;
 }
 
-// if this is a US county, use the qs:a2_alt for county
-// eg - wof:name = 'Lancaster' and qs:a2_alt = 'Lancaster County', use latter
-function getName(wofData) {
-  if (isUsCounty(wofData)) {
-    //console.log('using qs:a2_alt name');
-    return wofData.properties['qs:a2_alt']
-  }
+function getLocalizedName(wofData, langProperty) {
 
-  if (wofData.properties.hasOwnProperty('wof:lang_x_official') && !_.isEmpty(wofData.properties['wof:lang_x_official'])) {
+  if (wofData.properties.hasOwnProperty(langProperty) && !_.isEmpty(wofData.properties[langProperty])) {
 
     // build the preferred lang key to use for name, like 'name:deu_x_preferred'
-    var official_lang_key = getOfficialLangName(wofData);
-
-    //console.log('has official lang: ', official_lang_key, wofData.properties['wof:name'], wofData.properties['wof:placetype'], wofData.properties['wof:id']);
+    var official_lang_key = getOfficialLangName(wofData, langProperty);
 
     // check if that language is available
     if (wofData.properties.hasOwnProperty(official_lang_key)) {
 
-      //console.log('using official lang name: ', wofData.properties[official_lang_key], wofData.properties['wof:placetype'], wofData.properties['wof:id']);
+      logger.debug(langProperty, '[using]', wofData.properties[official_lang_key],
+        wofData.properties['wof:placetype'], wofData.properties['wof:id']);
+
+      if (wofData.properties['wof:name'] !== wofData.properties[official_lang_key][0]) {
+        logger.debug('name !== official', wofData.properties['wof:name'],
+          wofData.properties[official_lang_key], official_lang_key);
+      }
 
       // return the right name if all went well
-      return wofData.properties[official_lang_key];
+      return wofData.properties[official_lang_key][0];
+    }
+    else {
+      logger.debug(langProperty, '[missing]', official_lang_key, wofData.properties['wof:name'],
+        wofData.properties['wof:placetype'], wofData.properties['wof:id']);
     }
   }
+  return false;
+}
 
-  if (wofData.properties.hasOwnProperty('wof:label')) {
-    return wofData.properties['wof:label'];
+function getPropertyValue(wofData, property) {
+  if (wofData.properties.hasOwnProperty(property)) {
+    logger.debug(property, '[using]', wofData.properties['wof:placetype'],
+      wofData.properties['wof:id']);
+    return wofData.properties[property];
+  }
+  return false;
+}
+
+function getName(wofData) {
+
+  // if this is a US county, use the qs:a2_alt for county
+  // eg - wof:name = 'Lancaster' and qs:a2_alt = 'Lancaster County', use latter
+  if (isUsCounty(wofData)) {
+    return getPropertyValue(wofData, 'qs:a2_alt');
   }
 
-  //console.log('using default wof:name');
-  return wofData.properties['wof:name'];
+  // attempt to use the following in order of priority and fallback to wof:name if all else fails
+  return getLocalizedName(wofData, 'wof:lang_x_official') ||
+    getLocalizedName(wofData, 'wof:lang') ||
+    getPropertyValue(wofData, 'wof:label') ||
+    getPropertyValue(wofData, 'wof:name');
 }
