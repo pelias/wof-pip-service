4fbdcff7c7bd70871b64a70978c4fb653a240ece
Diana Shkolnikov <dianashk@gmail.com>
2016-11-17 09:54:29 -0500

    fix: country ISO codes needed to be propagated to client

--- test/functional.js
+++
@@ -1,2 +0,0 @@
-require('./testDependencyMode');
-require('./testLayerLookup');
--- test/testLayerLookup.js
+++ test/testLayerLookup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs');
 var path = require( 'path' );
 
@@ -23,37 +25,69 @@ var layers = [
   'region' // 4698
 ];
 
-createPIPService(layers, function (err, pipService) {
-  var basePath = path.resolve(__dirname);
-  var inputDataPath = basePath + '/data/layerTestData.json';
-  var inputData = require( inputDataPath );
-  var results = [];
-  var expectedPath = basePath + '/data/expectedLayerTestResults.json';
+function test(callback) {
+  createPIPService(layers, function (err, pipService) {
+    var basePath = path.resolve(__dirname);
+    var inputDataPath = basePath + '/data/layerTestData.json';
+    var inputData = require( inputDataPath );
+    var results = [];
+    var expectedPath = basePath + '/data/expectedLayerTestResults.json';
 
-  async.forEach(inputData, function (location, done) {
-    pipService.lookup(location.latitude, location.longitude, function (err, result) {
-      results.push(result);
-      done();
-    }, location.layers);
-  },
-  function end() {
-    var expected = JSON.parse(fs.readFileSync(expectedPath));
+    async.forEach(inputData, function (location, done) {
+      pipService.lookup(location.latitude, location.longitude, function (err, result) {
+        results.push(result);
+        done();
+      }, location.layers);
+    },
+    function end() {
+      var expected = JSON.parse(fs.readFileSync(expectedPath));
 
-    // uncomment this to write the actual results to the expected file
-    // make sure they look ok though. semicolon left off so jshint reminds you
-    // not to commit this line
-    //fs.writeFileSync(expectedPath, JSON.stringify(results, null, 2))
+      // uncomment this to write the actual results to the expected file
+      // make sure they look ok though. semicolon left off so jshint reminds you
+      // not to commit this line
+      //fs.writeFileSync(expectedPath, JSON.stringify(results, null, 2))
 
-    var diff = deep(expected, results);
+      var diff = deep(sortResults(expected), sortResults(results));
 
-    if (diff) {
-      console.log('expected and actual output are the same');
-      console.log(diff);
-      process.exit(1);
-    } else {
-      console.log('expected and actual output are the same');
-    }
+      pipService.end();
 
-    pipService.end();
+      if (diff) {
+        console.log('expected and actual output are different');
+        console.log(diff);
+      }
+      else {
+        console.log('expected and actual output are the same');       
+      }
+      callback();
+    });
   });
+
+  /**
+   * Sort result arrays which look like this
+   *
+   * [ [{Id:1}, {Id:2}, {Id:3}], [{Id:5}, {Id:6], [{Id:7}, {Id:8}, {Id:9}] ]
+   */
+  function sortResults(results) {
+    // sort the individual result arrays
+    results.forEach(function (a) {
+      a.sort(function (a, b) {
+        if (a.Id > b.Id) return 1;
+        if (a.Id < b.Id) return -1;
+        return 0;
+      });
+    });
+
+    results.sort(function (a, b) {
+      if (a[0].Id > b[0].Id) return 1;
+      if (a[0].Id < b[0].Id) return -1;
+      return 0;
+    });
+
+    return results;
+  }
+};
+
+
+test(function (err) {
+  process.exit(err);
 });
