e9bbe8cf1ce56a1190242ccc942e09deae50ba5e
Julian Simioni <julian@simioni.org>
2017-02-09 17:40:12 -0500

    Convert POSIX path from WOF to native paths
    This allows WOF admin data to be loaded on Windows.

--- src/components/loadJSON.js
+++ src/components/loadJSON.js
@@ -6,7 +6,11 @@ var fs = require('fs');
 module.exports.create = function(datapath) {
   // parse and return JSON contents
   return through.obj(function(record, enc, next) {
-    var filename = [ datapath, 'data', record.path ].join(path.sep);
+    // record.path is provided from WOF in POSIX path format
+    // use the path.posix tools to calculate the directory tree
+    var record_path = record.path.split(path.posix.sep);
+    // then use the OS specific path module to recombine them
+    var filename = [ datapath, 'data' ].concat(record_path).join(path.sep);
 
     try {
       this.push(JSON.parse(fs.readFileSync(filename)));
